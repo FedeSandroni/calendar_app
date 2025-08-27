104851c91bd14e6398b37073262a851edc95e027
Federico Sandroni <33227690+FedeSandroni@users.noreply.github.com>
2025-08-27 13:18:19 -0300

    project base & structure

---
+++ src/calendar/components/Navbar.tsx
@@ -0,0 +1,13 @@
+export const Navbar = () => {
+  return (
+    <div className="navbar navbar-dark bg-dark mb-4 px-4">
+      <span className="navbar-brand">
+        <i className="fas fa-calendar-alt"></i>
+        &nbsp; Federico
+      </span>
+      <button className="btn btn-outline-danger">
+        <i className="fa-solid fa-right-from-bracket"></i> <span>Salir</span>
+      </button>
+    </div>
+  );
+};
---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,19 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { LoginPage } from "../auth";
+import { CalendarPage } from "../calendar";
+
+export const AppRouter = () => {
+  let authStatus = "not-authenticated";
+  authStatus = "authenticated";
+  return (
+    <Routes>
+      {authStatus === "not-authenticated" ? (
+        <Route path="/auth/*" element={<LoginPage />}></Route>
+      ) : (
+        <Route path="/*" element={<CalendarPage />}></Route>
+      )}
+
+      <Route path="/*" element={<Navigate to="/auth/login" />}></Route>
+    </Routes>
+  );
+};

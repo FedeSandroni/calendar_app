a9ae72a879489c8aab3ee211a4b58f17d32353b1
Federico Sandroni <33227690+FedeSandroni@users.noreply.github.com>
2025-08-27 16:02:29 -0300

    view

--- src/calendar/pages/CalendarPage.tsx
+++ src/calendar/pages/CalendarPage.tsx
@@ -1,9 +1,45 @@
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import { addHours, format, parse, startOfWeek, getDay } from "date-fns";
+import { enUS } from "date-fns/locale/en-US";
 import { Navbar } from "..";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const locales = {
+  "en-US": enUS,
+};
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [
+  {
+    title: "Cumpleaños del jefe",
+    notes: "Hay que comprar el pastel",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: "#fafafa",
+    user: {
+      id: "123",
+      name: "Federico Sandroni",
+    },
+  },
+];
 
 export const CalendarPage = () => {
   return (
     <>
       <Navbar />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "calc(100vh - 80px)" }}
+      />
     </>
   );
 };
